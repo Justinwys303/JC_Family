270f5273a8cb6062922815108b23009a9e412240
wys <wuyushan1989@163.com>
2018-06-25 16:01:52 +0800

    登录数据库更改
    更改登录数据库

--- JC_Family/Models/IdentityModels.cs
+++ JC_Family/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace JC_Family.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("name=JC_FamilyDB", throwIfV1Schema: false)
         {
         }
 
